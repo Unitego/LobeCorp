14fdc365cb880a14fc0e09c682d20bb6ee4f4a93
baka4n <qq474899581@gmail.com>
2025-08-06 15:02:10 +0800

    去除重定向导致的其他模组兼容性问题

--- src/main/java/net/unitego/lobecorp/mixin/HungerMobEffectMixin.java
+++ src/main/java/net/unitego/lobecorp/mixin/HungerMobEffectMixin.java
@@ -1,5 +1,6 @@
 package net.unitego.lobecorp.mixin;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import net.minecraft.world.effect.HungerMobEffect;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,14 +9,18 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(HungerMobEffect.class)
 public abstract class HungerMobEffectMixin {
+
     //重定向饥饿效果，让他直接调用饥饿机制增加消耗度方法，防止饥饿效果能影响到干渴机制
-    @Redirect(method = "applyEffectTick", at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/world/entity/player/Player;causeFoodExhaustion(F)V"))
-    private void applyEffectTickMixin(Player instance, float exhaustion) {
-        if (!instance.getAbilities().invulnerable) {
-            if (!instance.level().isClientSide) {
-                instance.getFoodData().addExhaustion(exhaustion);
-            }
-        }
+    @WrapWithCondition(
+            method = "applyEffectTick",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/player/Player;causeFoodExhaustion(F)V")
+    )
+    private boolean applyEffectTickCondition(Player player, float exhaustion) {
+        return !player.getAbilities().invulnerable && !player.level().isClientSide();
     }
+
+
+
+
 }
